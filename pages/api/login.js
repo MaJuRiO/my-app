b80f80862d1b61d0d7f09358ab14c4fc8d9800ec
MaJuRiO <tadpong.majurio@mail.kmutt.ac.th>
2023-07-04 15:16:53 +0700

    update status db when connect

--- pages/api/login.js
+++ pages/api/login.js
@@ -5,6 +5,7 @@ export default async (req, res) => {
     try {
         const client = await clientPromise;
         const db = client.db("account");
+        console.log("Mongo Connected")
         const { Email, password: pass } = req.body;
 
         const checkUsername = await db.collection("posts").findOne({Email:Email});
